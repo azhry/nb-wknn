a921eaa7eae04d1eef5c079387775a26d46db3de
azhry <arliansyah_azhary@yahoo.com>
2019-01-11 00:59:30 +0700

    Added UI

--- src/Entity/ExcelHandler.java
+++ src/Entity/ExcelHandler.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.ss.usermodel.Row;
@@ -29,8 +28,39 @@ public class ExcelHandler {
         
     }
     
-    public List<Patient> read(String path, int sheetIndex) throws FileNotFoundException, IOException {
+    public List<Patient> read(String path, int sheetIndex)
+            throws FileNotFoundException, IOException {
         File excelFile = new File(baseDir + path);
+        FileInputStream fis = new FileInputStream(excelFile);
+        
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFSheet sheet = workbook.getSheetAt(sheetIndex);
+        List<Patient> result = new ArrayList<>();
+        
+        Iterator<Row> rowIt = sheet.iterator();
+        if (rowIt.hasNext()) {
+            rowIt.next();
+        }
+        
+        while (rowIt.hasNext()) {
+            Row row = rowIt.next();
+            result.add(new Patient(row.getCell(0).getStringCellValue(),
+                    row.getCell(1).getStringCellValue().charAt(0), 
+                    (int)row.getCell(2).getNumericCellValue(), 
+                    (int)row.getCell(3).getNumericCellValue(), 
+                    (int)row.getCell(4).getNumericCellValue(), 
+                    (int)row.getCell(5).getNumericCellValue(), 
+                    (int)row.getCell(6).getNumericCellValue(), 
+                    (int)row.getCell(7).getNumericCellValue(), 
+                    row.getCell(8).getStringCellValue()));
+        }
+        
+        return result;
+    }
+    
+    public List<Patient> read(File file, int sheetIndex)
+            throws FileNotFoundException, IOException {
+        File excelFile = file;
         FileInputStream fis = new FileInputStream(excelFile);
         
         XSSFWorkbook workbook = new XSSFWorkbook(fis);
